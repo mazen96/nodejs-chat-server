e6277d54627d3f1ad914074e21379321924b3d30
Mazen <mazen.mohamed.967@gmail.com>
2020-07-07 00:25:02 +0200

    Implement basic socket functionalities

--- index.js
+++ index.js
@@ -9,10 +9,24 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+// Socket handling ////////////////////////////////////////////
+
+io.on("connection", (socket) => {
+    console.log("Server:: New connection initiated from -client- to -server- -- *connection*");
+
+    // Manage that specific #Socket# connection //////////////
+
+    socket.on("disconnect", () => {
+        console.log("Server:: user left connction -- *disconnect*");
+    });
+    
+});
+
+///////////////////////////////////////////////////////////////
+
 // Middlewares/////////////////////////////////////////////////
 app.use(router);
 ///////////////////////////////////////////////////////////////
-
 
 server.listen(PORT, () => console.log(`Server started and listening on port number: ${PORT}`));
 
